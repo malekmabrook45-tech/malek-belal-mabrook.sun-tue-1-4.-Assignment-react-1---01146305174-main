329eeff47e7afef08b5e854a20fb918249040cf1
malekmabrook45-tech <malekmabrook45@gmail.com>
2026-02-23 18:51:13 +0200

    Fix: add base path for vite deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/malek-belal-mabrook.sun-tue-1-4.-Assignment-react-1---01146305174-main/"
 })
